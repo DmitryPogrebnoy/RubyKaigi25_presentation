b2987c6b2cc8ccfd7dc2a648d873fcd4715b64cc
Dmitry Pogrebnoy <dmitry.pogrebnoy@jetbrains.com>
2025-03-16 21:49:35 +0100

    Adjust puts_debuggerer example and debuggers

---
+++ src/presentation_samples/1_example_with_bug/6_example_define_debug.rb
@@ -0,0 +1,15 @@
+require 'debug'
+
+def process(thing)
+  binding.break
+  if defined? thing.to_s || defined? thing.inspect
+    puts "Element is Printable"
+  else
+    puts "Element is Not Printable"
+  end
+end
+
+process(5) # => Element is Printable
+process(BasicObject.new) # => Element is Printable
+
+
